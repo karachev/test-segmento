b5be1a8b86c6b6f34034c6336cb6d4a2a2221b10
Pavel Karachev <karachevpavel@gmail.com>
2018-02-01 16:01:50 +0300

    Добавляет уникальное заполнение ID

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
 'use strict';
 
 let buttonAdd = document.querySelector('.button-add');
-let table = document.querySelector('table');
+let tableBody = document.querySelector('tbody');
 let result = document.querySelector('#result');
 let balances = document.querySelectorAll('.balance');
-let resultValue = 0;
+
+let countID = document.querySelectorAll('input[title="id"]').length;
+
+// const MAX_LENGTH_COMMENT = 512;
+
 
 getBalance();
 
 buttonAdd.addEventListener('click', function () {
+  countID++;
   let tr = document.createElement('tr');
-  tr.innerHTML = "<td><input title=\"id\" type=\"number\" value=\"\"></td>" + "<td><input title=\"Количество средств\" type=\"number\" value=\"\"></td>" + "<td><input title=\"Комментарий\" type=\"text\" value=\"\" maxlength=\"512\"></td>";
-  table.appendChild(tr);
-  tr.querySelector('input[title="id"]').focus();
+  tr.innerHTML = `<td><input title=\"id\" type=\"number\" value=\"${countID}\" disabled></td>` +
+                  `<td><input title=\"Количество средств\" class=\"balance\" type=\"number\" value=\"\"></td>` +
+                  `<td><input title=\"Комментарий\" type=\"text\" value=\"\" maxlength=\"512\"></td>`;
+  tableBody.appendChild(tr);
+  tr.querySelector('.balance').focus();
 });
 
 function getBalance() {
+  let resultValue = 0;
   for (let i = 0; i < balances.length; i++) {
     resultValue += +balances[i].value;
   }
   result.innerHTML = resultValue.toString();
 }
 
+// function validationComment() {
+//
+// }
+
 // TODO Реализованная возможность сортировки по полям "Id" и "Количество средств"
 // TODO Обновление результата
 // TODO валидация ячеек
-// TODO id уникальное
-// TODO id не редактируется
 // TODO комментарий ограничен на js 512 символов
 // TODO Сохранение состояния сортировки в url. Для возможности обмена ссылкой с заданной сортировкой
 // TODO Добавление pagination если количество записей в таблице превышает 10
