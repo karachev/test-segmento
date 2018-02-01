db1510be62b52cb774cf60f67ae5cbb37d03d234
Pavel Karachev <karachevpavel@gmail.com>
2018-02-01 15:15:49 +0300

    Добавляет TODO

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ buttonAdd.addEventListener('click', function () {
   let tr = document.createElement('tr');
   tr.innerHTML = "<td><input title=\"id\" type=\"number\" value=\"\"></td>" + "<td><input title=\"Количество средств\" type=\"number\" value=\"\"></td>" + "<td><input title=\"Комментарий\" type=\"text\" value=\"\" maxlength=\"512\"></td>";
   table.appendChild(tr);
-  // debugger;
-  tr.children[0].children[0].focus();
+  tr.querySelector('input[title="id"]').focus();
 });
 
 function getBalance() {
@@ -23,3 +22,12 @@ function getBalance() {
   result.innerHTML = resultValue.toString();
 }
 
+// TODO Реализованная возможность сортировки по полям "Id" и "Количество средств"
+// TODO Обновление результата
+// TODO валидация ячеек
+// TODO id уникальное
+// TODO id не редактируется
+// TODO комментарий ограничен на js 512 символов
+// TODO Сохранение состояния сортировки в url. Для возможности обмена ссылкой с заданной сортировкой
+// TODO Добавление pagination если количество записей в таблице превышает 10
+// TODO Кроссбраузерность и адаптивность
