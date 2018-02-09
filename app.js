11ab81f175c52f58f46b08e770dc6b5eb0d49df9
Pavel Karachev <karachevpavel@gmail.com>
2018-02-09 17:21:42 +0300

    Правит оишбку при заполнение превышающую валидацию

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ function validationComment() {
 function createTable() {
   for (let i = 1; i <= 15; i++) { // Наичнается с единицы, чтобы id и balance != 0
     let initialData = document.createElement('tr');
-    let valueTd = (i % 2) ? i : i * 100;
+    let valueTd = (i % 2) ? i : i * 10;
     initialData.innerHTML = `<td><input title=\"id\" type=\"number\" value=\"${i}\" disabled></td>` +
       `<td><input title=\"Количество средств\" class=\"balance\" type=\"number\" value=\"${valueTd}\"></td>` +
       `<td><input title=\"Комментарий\" class=\"comment\" type=\"text\" value=\"Комментарий\" maxlength=\"512\"></td>`;
